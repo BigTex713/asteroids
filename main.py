30b1f1f20998b514855cc1f37e95cd5ae8b0474e
BigTex713 <ryancalp@gmail.com>
2025-10-13 21:25:18 -0400

    added .clock method to game

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     pygame.init()
     
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -22,6 +24,10 @@ def main():
         black = (0,0,0)
         screen.fill(black)
         pygame.display.flip()
+        
+        
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
