3cd27c960ca858806ce8d0e8e705a4533fb53b42
BigTex713 <ryancalp@gmail.com>
2025-10-19 12:02:21 -0400

    added player movement

--- constants.py
+++ constants.py
@@ -1,14 +1,17 @@
 #This File is used to store constant variables
 #relted to the asteroids game
 
+#Screen Settings
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
+#steroid Constants
 ASTEROID_MIN_RADIUS = 20
 ASTEROID_KINDS = 3
 ASTEROID_SPAWN_RATE = 0.8 #SECONDS
 ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
 
+#Player Constants
 PLAYER_RADIUS = 20
-
 PLAYER_TURN_SPEED = 300
+PLAYER_SPEED = 200
